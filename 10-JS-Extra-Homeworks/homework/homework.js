0c8f8d8ee2879c912fca02184f45861cdb952989
Ivatroth <ivana.frontroth@gmail.com>
2022-10-19 07:16:40 -0300

    Commit de JSX

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,19 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+ 
+  var matriz=[]
+  for(let clave in objeto){
+    matriz.push([clave, objeto[clave]]);
+   
+  }
+  return matriz
+
+  //FUNCIONA:
+  //PEEEERO HAY UN Metodo de Objet mas fasil, QUE DEBUEVE LO MISMO EN UN SOLO PASO
+  //AQUI ABAJO LO DEJO
+  //return  Object.entries(objeto);
+  
 }
 
 
@@ -18,6 +31,19 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+ 
+  //Creo el objeto con los caracteres no repetidos.
+  var obj={};
+  for(i=0; i<string.length; i++){
+    obj[string[i]] =0 ;
+  }
+  //ahora cuento cuantos hay de cada uno NOTA: NO ME DEJABA HACERLO DE UNA VEZ
+  //POR ESO TUVE QUE REPETIR EL FOR
+  for(i=0; i<string.length; i++){
+    obj[string[i]] ++ ;
+  }
+   
+  return obj;
 }
 
 
@@ -26,6 +52,21 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var s2=s.toUpperCase();
+  var s3=s.toLowerCase();
+  var may='';
+  var min='';
+  for (i=0; i<s.length; i++){
+    if(s2.includes(s.charAt(i))){
+      may=may.concat(s.charAt(i));
+    }else if (s3.includes(s.charAt(i))){
+      //return 'entro por else'
+      min=min.concat(s.charAt(i));
+    }
+  }
+
+  return may+min;
+
 }
 
 
@@ -35,6 +76,21 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  
+  //transformo la frase en array de palabras
+  var frase = str.split(' ');
+
+  // itero en el array y trabajo en cada palabra, primero la convierto en array (split)
+  // luego la invierto (reverse) y luego la uno de nuevo en una palabra (join)
+  // voy concatenando cada una y luego la retorno
+  var salida= '';
+  for(i=0; i<frase.length; i++){
+    var a = frase[i].split('');
+    var b = a.reverse();
+    var c = b.join('');
+    salida += c +' '; 
+  }
+  return salida.trimEnd();
 } 
 
 
@@ -43,6 +99,29 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+/*
+  // transformo el numero en string
+  var a = numero.toString();
+  // luego al string lo hago array
+  var b = a.split('');                //PARECE QUE PODIA HACER TODO EN UNA SOLA LINEA
+  // luego lo invierto                // numero.toString().split('').reverse().join('');
+  var c = b.reverse();                // y a la mm....
+  // luego lo vuelvo cadena de nuevo
+  var d = c.join('');
+
+  if (a === d){ return "Es capicua";}   // TAMBIEN PODIA HACER TODO DENTRO DEL IF
+  else {return "No es capicua";}
+*/
+
+// TODO LO ANTERIOR SE RESUMIO EN LO SIGUIENTE :)
+
+  if(numero === Number(numero.toString().split('').reverse().join(''))){ 
+    return "Es capicua";
+  }
+  else {
+    return "No es capicua";
+  }
+
 }
 
 
@@ -50,13 +129,35 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
-}
 
+  var a = cadena.replaceAll('a','');
+  var b = a.replaceAll('b', '');
+  var c = b.replaceAll('c', '');
+  
+  return c;
+}
 
 function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+
+  //ESTA ME COSTO UN MONTON PORQUE QUERIA HACER YO
+  //LA LOGICA DE ORDENACION, PEERO NO ME SALIA
+  //ASI QUE APRENDI COMO USAR SORT Y LISTO
+  arr.sort( (a,b) => {
+    if (a.length<b.length) {
+      return -1;
+    }
+    if (a.length>b.length) {
+      return 1;
+    }
+    // a debe ser igual b
+    return 0;
+  });
+
+  return arr;
+
 }
 
 
@@ -66,6 +167,14 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+
+  var arr2=[];
+  for(i=0; i<arreglo1.length; i++){
+    if(arreglo2.includes(arreglo1[i])){
+      arr2.push(arreglo1[i])
+    }
+  }
+  return arr2;
 }
 
 
